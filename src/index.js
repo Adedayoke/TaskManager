7139c5ab2fe8ee35ffbd089cc9c70f3d9a75f7b2
Adedayoke <adedayoke2006@gmail.com>
2023-02-09 22:23:26 +0100

    firebase complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
+import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import {getFirestore, doc, setDoc, updateDoc, arrayUnion, getDoc} from 'firebase/firestore';
 'use script';
 
 const enter = document.querySelector('.enter')
@@ -9,6 +9,13 @@ const tasks = document.querySelector('.tasks')
 const dateNow = new Date();
 const overlayCont = document.querySelector(".overlayCont")
 const overlay = document.querySelector(".overlay")
+const loginCont = document.querySelector(".loginCont")
+const signupCont = document.querySelector(".signupCont")
+const name = document.querySelectorAll(".name")
+const userName = document.querySelectorAll(".userName")
+const error = document.querySelector(".error")
+const ham = document.querySelector(".ham")
+
 
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
@@ -28,154 +35,77 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
-let user = ""
+let currentuser;
+currentuser = JSON.parse(localStorage.getItem("currentuser"))
+let savedUser = {}
 
-// let dataValue = 0;
-// let dataValue2 = 0;
-// let taskState = false
-// let allTasks = []
-// let control = function(){
-//     const task = `
-//     <div class="eachTask " data-index = ${dataValue}>
-//         <div class="first1"><p class="ent">${enter.value}</p>
-//             <p class="first2">${dateNow.getDate()}/${dateNow.getMonth() + 1}/${dateNow.getFullYear()}</p>
-//         </div>
-//         <div class="times">
-//             <img src="./edit2.png" alt="" class="edit2">
-//             <img class ="bin" src="./trash.png" alt="">
-//         </div>
-//     </div>`;
-//     const read = `<button class="rem">Edit Task</button>`
-//     const amendment = `
-//         <div class="shadow">
-//             <div class="inq">
-//                 <div class="time">&times;</div>
-//                 <p class="del">DELETE TASK</p>
-//                 <p class="con">ARE YOU SURE YOU WANT TO DELETE THIS TASK ?</p>
-//                 <div class="buttons">
-//                     <div class="but">
-//                         <button class="yes">YES</button>
-//                         <button class="no">NO</button>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>`
-//     const orig = `<button class="add">Add Task</button>`
-//     dataValue++;
-//     whole.insertAdjacentHTML("beforeend", task)
-//     const TASKS = document.querySelectorAll(".eachTask")
-//     allTasks = TASKS
-//     if (allTasks.length > 0) {
-//         appear.classList.add("none")
-//         console.log("hello")
-//     }else{
-//         appear.classList.remove("none")
-//     }
-//     enter.value = ''
-//     inside.classList.add('bod')
-//     setTimeout(() => {ted.classList.remove('non')}, 500);
-//     setTimeout(() => {ted.classList.add('non')}, 1000);
-//     const binAll = document.querySelectorAll('.bin');
-//     let currentBtn;
-//     binAll.forEach((bin) => {
-//         bin.addEventListener('click', (e) => {
-//             e.stopImmediatePropagation();
-//             // const parentVal = bin.parentElement.parentElement.dataset.index
-//             // enter.value = parentVal
-//             // .Remove()  or .RemoveChild() ...
-//             console.log(allTasks)
-//             currentBtn = bin.parentElement.parentElement;
-//             overlay.classList.remove('none')
-//             amend.classList.remove('non')
-//             amend.innerHTML = amendment
-//             const yes = document.querySelector('.yes')
-//             const no = document.querySelector('.no')
-//             const shadow = document.querySelector('.shadow')
-//             const time = document.querySelector('.time')
-//             yes.addEventListener('click', () => {
-//                 currentBtn.remove();
-//                 setTimeout(() => {suc.classList.remove('non')}, 500);
-//                 setTimeout(() => {suc.classList.add('non')}, 1000)
-//                 shadow.remove()
-//                 overlay.classList.add('none')
-//                 amend.classList.add('non')
-//             })
-//             no.addEventListener('click', function(){
-//                 shadow.remove()
-//                 overlay.classList.add('none')
-//                 amend.classList.add('non')
-//             })
-//             time.addEventListener('click', function(){
-//                 shadow.remove()
-//                 overlay.classList.add('none')
-//                 amend.classList.add('non')
-//             })
-//         })
-//     })
-//     const editAll = document.querySelectorAll('.edit2');
-//     editAll.forEach((edit) => {
-//         edit.addEventListener('click', (e) => {
-//             e.stopImmediatePropagation();
-//             currentBtn = edit.parentElement.parentElement;
-//             const reach = currentBtn.querySelector('.ent')
-//             enter.value = reach.textContent
-//             add.remove();
-//             adds.innerHTML = read
-//             const rem = document.querySelector('.rem')
-//             reach.classList.add('extra')
-//             rem.addEventListener('click', function(){
-//                 reach.textContent = enter.value
-//                 rem.remove();
-//                 adds.innerHTML = orig
-//                 const adding = document.querySelector('.add')
-//                 adding.addEventListener('click', function(){
-//                     if(enter.value !== ''){
-//                         control();
-//                     }
-//                 })
-//                 ted.textContent = 'Task has been edited successfully'
-//                 enter.value = ''
-//                 setTimeout(() => {ted.classList.remove('non')}, 500);
-//                 setTimeout(() => {ted.classList.add('non')}, 1000); 
-//                 reach.classList.remove('extra') 
-//             })
-//         })
-//     })
-// }
-// add.addEventListener('click', function(){
-//     if(enter.value !== ''){
-//         control();
-        
-//     }
-// })
+
+// localStorage.setItem('user', JSON.stringify(user))
+// localStorage.setItem('num', JSON.stringify(number))
+
+
 let dataValue = 0;
-let allTasks = [];
+let allTasks=[]
+let myTasks = [];
+const baseData = []
 let editedTask = 0
 let taskCont = ""
 
-if(allTasks.length === 0){
-    tasks.innerHTML = `<div class="appear">Tasks Appear Here</div>`
+
+window.onload = async()=>{
+    checkuser()
+    if(currentuser){
+        updateData()
+        
+    }
 }
-const addTask = ()=> {
-    for (let i = 0; i < allTasks.length; i++) {
-        const task = allTasks[i];
-        tasks.insertAdjacentHTML('beforeend',
-            `<div class="eachTask " data-index = ${i}>
+
+
+const updateData = async ()=>{
+    
+    const docRef = doc(db, "users", currentuser.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            allTasks = docSnap.data().allTasks
+            if(allTasks.length === 0){
+                tasks.innerHTML = `<div class="appear">Tasks Appear Here</div>`
+            }
+            addTask()
+        } else {
+        // doc.data() will be undefined in this case
+    }   
+}
+
+const addTask = async()=> {
+    if (currentuser){
+            await setDoc(doc(db, "users", currentuser.uid), {allTasks})
+            for (let i = 0; i < allTasks.length; i++) {
+                const task = allTasks[i];
+                tasks.insertAdjacentHTML('beforeend',
+                `<div class="eachTask " data-index = ${i}>
                 <div class="first1"><p class="ent">${task.taskName}</p>
                 <p class="first2">${task.date}</p>
                 </div>
                 <div class="times">
-                    <img src="./edit2.png" alt="" class="edit2">
-                    <img class ="bin" src="./trash.png" alt="">
+                <img src="./edit2.png" alt="" class="edit2">
+                <img class ="bin" src="./trash.png" alt="">
                 </div>
-        </div>`)
-        const binAll = document.querySelectorAll('.bin');
-        const editAll = document.querySelectorAll('.edit2');
-        binFunction(binAll)
-        editFunction(editAll)
+                </div>`)
+                const binAll = document.querySelectorAll('.bin');
+                const editAll = document.querySelectorAll('.edit2');
+                binFunction(binAll)
+                editFunction(editAll)
+            
+        }
+    }
+    else{
+        signupCont.classList.remove("none")
     }
 }
+
 const editTask = (editedTask)=>{
+    // console.log(number)
     allTasks.forEach((task)=>{
         if (task.id === editedTask){
             task.taskName = enter.value
@@ -213,7 +143,7 @@ const binFunction = (binAll)=>{
     })
 }
 const handleYes = (yes)=>{
-    yes.onclick = ()=>{
+    yes.onclick = async()=>{
         overlayCont.classList.add("none")
         const newTask = allTasks.filter((task)=>Number(taskCont.dataset.index) !== task.id)
         newTask.forEach(task =>{
@@ -221,6 +151,7 @@ const handleYes = (yes)=>{
             dataValue++
         })
         allTasks = newTask
+        await updateDoc(doc(db, "users", currentuser.uid), {allTasks})
         tasks.innerHTML= ""
         addTask()
     }
@@ -229,16 +160,6 @@ const handleNo = (no)=>{
     no.onclick = ()=>{
         overlayCont.classList.add("none")
     }
-}
-const deleteFunction =(taskCont) =>{
-    const newTask = allTasks.filter((task)=>Number(taskCont.dataset.index) !== task.id)
-    newTask.forEach(task =>{
-        task.id = dataValue
-        dataValue++
-    })
-    allTasks = newTask
-    tasks.innerHTML= ""
-    addTask()
 }
 const editFunction = (editAll) => {
     editAll.forEach((edit) => {
@@ -280,3 +201,156 @@ window.onkeydown = ({key})=>{
     }
 }
 
+
+
+// Login and sign up functions
+const signupForm = document.querySelector(".signupForm")
+const loginForm = document.querySelector(".loginForm")
+const logins = document.querySelectorAll(".only-login")
+const signin = document.querySelectorAll(".btn-login")
+const signups = document.querySelectorAll(".only-signup")
+const signup = document.querySelectorAll(".btn-signup")
+let email = "";
+let password = "";
+const signout = document.querySelectorAll(".btn-logout")
+const loginwithgoogle = document.querySelector(".--btn-danger")
+
+logins.forEach((login)=>{
+    login.onclick =()=>{
+        loginCont.classList.remove("none")
+        signupCont.classList.add("none")
+    }
+})
+signups.forEach((signup)=>{
+    signup.onclick =()=>{
+        signupCont.classList.remove("none")
+        loginCont.classList.add("none")
+    }
+})
+
+signupForm.onsubmit = (e)=>{
+    e.preventDefault();
+    const inputs = e.target.querySelectorAll('input')
+    email = inputs[0].value
+    password= inputs[1].value
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    //Signed in 
+    const user = userCredential.user;
+    localStorage.setItem('currentuser', JSON.stringify(user))
+    currentuser = JSON.parse(localStorage.getItem("currentuser"))
+    signupCont.classList.add("none")
+    loginCont.classList.add("none")
+    checkuser()
+    //...
+    })
+    .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    error.classList.remove("transformer")
+    setTimeout(() => {
+        error.classList.add("transformer")
+    }, 1500);
+    //..
+    });
+}
+loginForm.onsubmit = (e)=>{
+    e.preventDefault();
+    const inputs = e.target.querySelectorAll('input')
+    email = inputs[0].value
+    password= inputs[1].value
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    //Signed in 
+    const user = userCredential.user;
+    localStorage.setItem('currentuser', JSON.stringify(user))
+    currentuser = JSON.parse(localStorage.getItem("currentuser"))
+    signupCont.classList.add("none")
+    loginCont.classList.add("none")
+    checkuser()
+    //...
+    })
+    .catch((eror) => {
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // console.log(errorMessage)
+    error.classList.remove("transformer")
+    setTimeout(() => {
+        error.classList.add("transformer")
+    }, 1500);
+    //..
+    });
+}
+signout.forEach((out)=>{
+    out.onclick=()=>{
+        signOut(auth).then(() => {
+            const user = null
+            // Sign-out successful.
+            localStorage.setItem('currentuser', JSON.stringify(user))
+            currentuser = JSON.parse(localStorage.getItem("currentuser"))
+            loginCont.classList.remove("none")
+            checkuser()
+            
+          }).catch((error) => {
+            // An error happened.
+          });
+    }
+})
+//Auth
+console.log(currentuser)
+const checkuser = ()=>{
+    if(currentuser){
+        name.forEach((nam)=>{
+            nam.classList.remove("none")
+        })
+        userName.forEach((nam)=>{
+            nam.textContent = (currentuser.email.substring(0, currentuser.email.indexOf("@"))).charAt(0).toUpperCase() + currentuser.email.substring(0, currentuser.email.indexOf("@")).slice(1)
+        })
+        signin.forEach((gnin)=>{
+            gnin.classList.add("none")
+        })
+        signout.forEach((out)=>{
+            out.classList.remove("none")
+        })
+        signup.forEach((up)=>{
+            up.classList.add("none")
+        })
+    }
+    else{
+        signin.forEach((gnin)=>{
+            gnin.classList.remove("none")
+        })
+        signout.forEach((out)=>{
+            out.classList.add("none")
+        })
+        signup.forEach((up)=>{
+            up.classList.remove("none")
+        })
+    }
+}
+ham.onclick = ()=>{
+    ham.classList.toggle('rotate')
+    document.querySelector('.btns2').classList.toggle('hamTrans')
+}
+// sign in with google
+// const provider = new GoogleAuthProvider();
+// const signInWithGoogle = async()=>{
+//     await signInWithPopup(auth, provider)
+//     .then((result) => {
+//         const user = result.user;
+//         localStorage.setItem('currentuser', JSON.stringify(user))
+//         currentuser = JSON.parse(localStorage.getItem("currentuser"))
+//         signupCont.classList.add("none")
+//         loginCont.classList.add("none")
+//         checkuser()
+        
+//     }).catch((error) => {
+//         // Handle Errors here.
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         console.log(errorCode)
+//     });
+// }
+// loginwithgoogle.onclick = () => signInWithGoogle()
